eec633b44ead6f3d711d71d7b26b48f6e22c6ec7
Karel <kmarrero@visualaimsoft.com>
2023-07-14 17:49:17 -0500

    Spinning docker container for docker

---
+++ IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerApiFactory.cs
@@ -0,0 +1,56 @@
+﻿using Customers.Api;
+using Customers.Api.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace Customer.Api.Tests.Integration
+{
+    public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+    {
+
+        private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+            .WithDatabase("mydb")
+            .WithUsername("postgres")
+            .WithPassword("password")
+            .Build();
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+            });
+
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(IDbConnectionFactory));
+                services.AddSingleton<IDbConnectionFactory>(_ => 
+                    new NpgsqlConnectionFactory("Server=localhost;Port=5555;Database=mydb;User ID=postgres;Password=password;"));
+            });
+        }
+
+
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+        }
+
+
+        public new async Task DisposeAsync()
+        {
+            await _dbContainer.DisposeAsync();
+        }
+    }
+}
---
+++ IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Customers.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Customer.Api.Tests.Integration.CustomerController
+{
+    public class CreateCustomerControllerTests : IClassFixture<CustomerApiFactory>
+    {
+
+        private readonly CustomerApiFactory _apiFactory;
+
+        public CreateCustomerControllerTests(CustomerApiFactory apiFactory)
+        {
+            _apiFactory = apiFactory;
+        }
+
+        [Fact]
+        public async Task Test()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
