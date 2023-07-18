6539a81a36838b184554d156b4b29dc8c48727b6
kmarreroe <karel.marrero@thoughtworks.com>
2023-07-18 09:20:59 -0500

    Mock gihub api with wiremock

--- IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerApiFactory.cs
+++ IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerApiFactory.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +20,16 @@ namespace Customer.Api.Tests.Integration
     public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
     {
 
+        public const string ValidGithubUser = "validuser";
+
         private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:alpine")
             .WithDatabase("mydb")
             .WithUsername("postgres")
             .WithPassword("password")
             .Build();
+
+        private readonly GitHubApiServer _gitHubApiServer = new();
 
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -36,14 +42,27 @@ namespace Customer.Api.Tests.Integration
             builder.ConfigureTestServices(services =>
             {
                 services.RemoveAll(typeof(IDbConnectionFactory));
-                services.AddSingleton<IDbConnectionFactory>(_ => 
-                    new NpgsqlConnectionFactory("Server=localhost;Port=5555;Database=mydb;User ID=postgres;Password=password;"));
+                services.AddSingleton<IDbConnectionFactory>(_ =>
+                    // new NpgsqlConnectionFactory("Server=localhost;Port=5555;Database=mydb;User ID=postgres;Password=password;"));
+                    new NpgsqlConnectionFactory(_dbContainer.GetConnectionString()));
+
+                services.AddHttpClient("GitHub", httpClient =>
+                {
+                    httpClient.BaseAddress = new Uri(_gitHubApiServer.Url);
+                    httpClient.DefaultRequestHeaders.Add(
+                        HeaderNames.Accept, "application/vnd.github.v3+json");
+                    httpClient.DefaultRequestHeaders.Add(
+                        HeaderNames.UserAgent, $"Course-{Environment.MachineName}");
+                });
+
             });
         }
 
 
         public async Task InitializeAsync()
         {
+            _gitHubApiServer.Start();
+            _gitHubApiServer.SetUpUser(ValidGithubUser);
             await _dbContainer.StartAsync();
         }
 
@@ -51,6 +70,7 @@ namespace Customer.Api.Tests.Integration
         public new async Task DisposeAsync()
         {
             await _dbContainer.DisposeAsync();
+            _gitHubApiServer.Dispose();
         }
     }
 }
--- IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
+++ IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
@@ -1,8 +1,16 @@
-﻿using Customers.Api;
+﻿using Bogus;
+using Customers.Api;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
+using SharpYaml;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -12,17 +20,83 @@ namespace Customer.Api.Tests.Integration.CustomerController
     public class CreateCustomerControllerTests : IClassFixture<CustomerApiFactory>
     {
 
-        private readonly CustomerApiFactory _apiFactory;
+        // private readonly CustomerApiFactory _apiFactory;
+        private readonly HttpClient _client;
+
+        private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+            .RuleFor(x => x.Email, faker => faker.Person.Email)
+            .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+            .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGithubUser)
+            .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
+
 
         public CreateCustomerControllerTests(CustomerApiFactory apiFactory)
         {
-            _apiFactory = apiFactory;
+            // _apiFactory = apiFactory;
+            _client = apiFactory.CreateClient();
         }
 
+
         [Fact]
-        public async Task Test()
+        public async Task Create_CreatesUser_WhenDataIsValid()
         {
-            await Task.Delay(3000);
+
+            // Arange
+            var customer = _customerGenerator.Generate();
+
+            // Act
+            var response = await _client.PostAsJsonAsync("customers", customer);
+
+            // Assert
+            var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+            customerResponse.Should().BeEquivalentTo(customer);
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            response.Headers.Location!.ToString().Should().Be($"http://localhost/customers/{customerResponse.Id}");
+
+        }
+
+
+        [Fact]
+        public async Task Create_ReturnsValidationError_WhenEmailIsInvalid()
+        {
+            // Arrange
+            const string invalidEmail = "invalid-email";
+            var customer = _customerGenerator.Clone()
+            .RuleFor(x => x.Email, invalidEmail).Generate();
+
+
+            // Act
+            var response = await _client.PostAsJsonAsync("customers", customer);
+
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var error = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+            error!.Status.Should().Be(400);
+            error.Title.Should().Be("One or more validation errors occurred.");
+            error.Errors["Email"][0].Should().Be($"{invalidEmail} is not a valid email address");
+        }
+
+
+        [Fact]
+        public async Task Create_ReturnsValidationError_WhenGithubUserDoesNotExists()
+        {
+            // Arrange
+            const string invalidUser = "invalid-user";
+            var customer = _customerGenerator.Clone()
+            .RuleFor(x => x.GitHubUsername, invalidUser).Generate();
+
+
+            // Act
+            var response = await _client.PostAsJsonAsync("customers", customer);
+
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var error = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+            error!.Status.Should().Be(400);
+            error.Title.Should().Be("One or more validation errors occurred.");
+            error.Errors["GitHubUsername"][0].Should().Be($"There is no GitHub user with username {invalidUser}");
         }
     }
 }
