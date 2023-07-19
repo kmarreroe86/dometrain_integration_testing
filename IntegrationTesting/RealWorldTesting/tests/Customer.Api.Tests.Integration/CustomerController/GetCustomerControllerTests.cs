6266b7c76799419d8d630ca5e30ce0b44e8f92fe
kmarreroe <karel.marrero@thoughtworks.com>
2023-07-18 19:16:56 -0500

    exercices integration test

---
+++ IntegrationTesting/RealWorldTesting/tests/Customer.Api.Tests.Integration/CustomerController/GetCustomerControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FluentAssertions;
+using Xunit;
+
+namespace Customer.Api.Tests.Integration;
+public class GetCustomerControllerTests : IClassFixture<CustomerApiFactory>
+{
+
+    private readonly HttpClient _client;
+
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+            .RuleFor(x => x.Email, faker => faker.Person.Email)
+            .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+            .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGithubUser)
+            .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
+
+
+    public GetCustomerControllerTests(CustomerApiFactory apiFactory)
+    {
+        // _apiFactory = apiFactory;
+        _client = apiFactory.CreateClient();
+    }
+
+
+    [Fact]
+    public async Task Get_ReturnsCustomer_WhenCustomerExists()
+    {
+
+        // Arange
+        var customer = _customerGenerator.Generate();
+        var createResponse = await _client.PostAsJsonAsync("customers", customer);
+        var createdCustomer = await createResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+
+        // Act
+        var response = await _client.GetAsync($"customers/{createdCustomer!.Id}");
+
+        // Assert
+        var retreivedCustomer = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+        retreivedCustomer.Should().BeEquivalentTo(createdCustomer);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    }
+
+    [Fact]
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
+    {
+        // Act
+        var response = await _client.GetAsync($"customers/{Guid.NewGuid()}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+}
